07609c62730438be6226d7e50c6e0438642d4060
Hamima <hhalim1@jhu.edu>
2023-12-13 11:34:38 -0500

    query bus route for parity with FE

--- src/gobble.py
+++ src/gobble.py
@@ -5,7 +5,7 @@ import pandas as pd
 import requests
 import sseclient
 
-from constants import STOPS
+from constants import STOPS, ROUTES
 from config import CONFIG
 import gtfs
 import disk
@@ -13,7 +13,7 @@ import util
 
 API_KEY = CONFIG["mbta"]["v3_api_key"]
 HEADERS = {"X-API-KEY": API_KEY, "Accept": "text/event-stream"}
-URL = "https://api-v3.mbta.com/vehicles?filter[route]=1"
+URL = f'https://api-v3.mbta.com/vehicles?filter[route]={",".join(ROUTES)}'
 
 
 def get_stop_name(stops_df, stop_id):
