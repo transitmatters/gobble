11db0b6b17f68d99972cbe0ce70a5786afa6d40a
Devin Matte <devinmatte@gmail.com>
2023-12-22 15:21:02 -0500

    Adding Commuter rail tracking!

--- src/gobble.py
+++ src/gobble.py
@@ -6,7 +6,7 @@ import sseclient
 import logging
 from ddtrace import tracer
 
-from constants import ROUTES_BUS
+from constants import ROUTES
 from config import CONFIG
 from event import process_event
 import gtfs
@@ -18,7 +18,7 @@ tracer.enabled = CONFIG["DATADOG_TRACE_ENABLED"]
 
 API_KEY = CONFIG["mbta"]["v3_api_key"]
 HEADERS = {"X-API-KEY": API_KEY, "Accept": "text/event-stream"}
-URL = f'https://api-v3.mbta.com/vehicles?filter[route]={",".join(ROUTES_BUS)}'
+URL = f'https://api-v3.mbta.com/vehicles?filter[route]={",".join(ROUTES)}'
 
 
 def main():
