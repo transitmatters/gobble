2fc0cbde3d5f509332514e6d280b7cd50fde52c7
Devin Matte <devinmatte@gmail.com>
2023-12-22 12:28:49 -0500

    Changing check dates in test

--- src/tests/test_util.py
+++ src/tests/test_util.py
@@ -7,8 +7,8 @@ def test_service_date():
     assert service_date(datetime(2023, 12, 15, 5, 45, 0)) == date(2023, 12, 15)
     assert service_date(datetime(2023, 12, 15, 7, 15, 0)) == date(2023, 12, 15)
     assert service_date(datetime(2023, 12, 15, 23, 59, 59)) == date(2023, 12, 15)
-    assert service_date(datetime(2023, 12, 15, 0, 0, 0)) == date(2023, 12, 14)
-    assert service_date(datetime(2023, 12, 15, 2, 59, 59)) == date(2023, 12, 14)
+    assert service_date(datetime(2023, 12, 16, 0, 0, 0)) == date(2023, 12, 15)
+    assert service_date(datetime(2023, 12, 16, 2, 59, 59)) == date(2023, 12, 15)
 
 
 def test_localized_datetime():
@@ -16,5 +16,5 @@ def test_localized_datetime():
     assert service_date(datetime(2023, 12, 15, 5, 45, 0, tzinfo=EASTERN_TIME)) == date(2023, 12, 15)
     assert service_date(datetime(2023, 12, 15, 7, 15, 0, tzinfo=EASTERN_TIME)) == date(2023, 12, 15)
     assert service_date(datetime(2023, 12, 15, 23, 59, 59, tzinfo=EASTERN_TIME)) == date(2023, 12, 15)
-    assert service_date(datetime(2023, 12, 15, 0, 0, 0, tzinfo=EASTERN_TIME)) == date(2023, 12, 14)
-    assert service_date(datetime(2023, 12, 15, 2, 59, 59, tzinfo=EASTERN_TIME)) == date(2023, 12, 14)
+    assert service_date(datetime(2023, 12, 16, 0, 0, 0, tzinfo=EASTERN_TIME)) == date(2023, 12, 15)
+    assert service_date(datetime(2023, 12, 16, 2, 59, 59, tzinfo=EASTERN_TIME)) == date(2023, 12, 15)
