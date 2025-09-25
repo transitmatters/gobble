cbd355974c926346b1002335a83bc61e9a904d03
Andrew Kouré <andrew@koure.org>
2025-09-25 16:21:57 +0000

    Change Occupancy Status Enum to int

--- src/Carriage.py
+++ src/Carriage.py
@@ -1,18 +1,17 @@
 import json
-from gtfs_types import OccupancyStatus
 
 
 class Carriage:
     def __init__(self, carriage_details):
         self.label: str = carriage_details.label
         self.carriage_sequence: int = carriage_details.carriage_sequence
-        self.occupancy_status: OccupancyStatus = carriage_details.occupancy_status
+        self.occupancy_status: int = carriage_details.occupancy_status
         self.occupancy_percentage: float | None = None
 
     def Update(self, carriage_details):
         self.label: str = carriage_details.label
         self.carriage_sequence: int = carriage_details.carriage_sequence
-        self.occupancy_status: OccupancyStatus = carriage_details.occupancy_status
+        self.occupancy_status: int = carriage_details.occupancy_status
         self.occupancy_percentage: float | None = None
 
     def toJSON(self):
